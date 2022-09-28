ed8faa27365554476000c9e41ded779b5e2d4f8f
Dmitri Astafiev <demius.md@gmail.com>
2022-09-28 16:19:51 +0300

    try add vaadin and another build

---
+++ src/core/startup/register-vaadin-elements.ts
@@ -0,0 +1,7 @@
+import '@vaadin/button'
+import '@vaadin/checkbox'
+import '@vaadin/icon'
+import '@vaadin/icons'
+import '@vaadin/notification'
+import '@vaadin/password-field'
+import '@vaadin/text-field'
